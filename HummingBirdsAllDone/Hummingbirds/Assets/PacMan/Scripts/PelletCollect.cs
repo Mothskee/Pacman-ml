de3cee00d461da5cf441670125f00a17d3c10c65
Mothskee <54814906+Mothskee@users.noreply.github.com>
2020-10-17 19:50:58 -0600

    Basic gameover/win state

--- HummingBirdsAllDone/Hummingbirds/Assets/PacMan/Scripts/PelletCollect.cs
+++ HummingBirdsAllDone/Hummingbirds/Assets/PacMan/Scripts/PelletCollect.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PelletCollect : MonoBehaviour
 {
+    public Text statusText;
+    public GameObject thePlayer;
+
     public int collected = 0;
     public int gameover = 0;
     [SerializeField]
-
+    private void Start()
+    {
+        statusText.text = " ";
+    }
 
     void Update()
     {
-
+        if (collected == 4)
+        {
+            statusText.text = "You Win!";
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -24,6 +34,8 @@ public class PelletCollect : MonoBehaviour
         if (other.gameObject.CompareTag("ghost"))
         {
             gameover++;
+            statusText.text = "Game Over";
+            thePlayer.transform.position = new Vector3(0, -100f, 0);
         }
      
     }
